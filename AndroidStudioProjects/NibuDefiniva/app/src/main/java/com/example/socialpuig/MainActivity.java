31aec1c92cc8cd1e7ab216873951c0ac0f87421e
Frnkp21 <perezcarvajalfrank@elpuig.xeill.net>
2024-03-04 22:26:40 +0100

    6

--- AndroidStudioProjects/NibuDefiniva/app/src/main/java/com/example/socialpuig/MainActivity.java
+++ AndroidStudioProjects/NibuDefiniva/app/src/main/java/com/example/socialpuig/MainActivity.java
@@ -106,4 +106,5 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
 }
